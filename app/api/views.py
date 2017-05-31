c71309a6fc580d5e898ae90feba3c72ae2273a1b
Joe <chaosbus@gmail.com>
2017-05-31 21:41:35 +0800

    今天又搞懂了些4

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for, flash
 from . import bp_api
-
+from .. import db
+from ..models import SubsOperation
 
 @bp_api.route('/')
 def index():
@@ -20,3 +21,9 @@ def pi():
     return render_template('api/pi.html', subtitle='RaspberryPi')
 
 
+@bp_api.route('/show')
+def show():
+    dat = SubsOperation.query.all()
+    print SubsOperation.query.count()
+
+    return render_template('api/show.html', table=dat)
